7e6b2151f909827cba4ba34dee9f09cbc5236b9b
Deep Lakkad <114099704+deeplakkad070@users.noreply.github.com>
2025-09-11 10:38:09 +0530

    Update server.js

--- crud-service/server.js
+++ crud-service/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {   
-  res.json({ message: "Welcome to Crud application." });
+  res.json({ message: "Welcome to crud_service application." });
 });
 
 app.use("/dev", router);
@@ -22,5 +22,6 @@ app.listen(8080, () => {
 
 
 export default app;
+
 
 
